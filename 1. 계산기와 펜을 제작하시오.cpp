76bb87042ba949384f3bbe5366ac4023b4df35bf
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-09-12 17:46:29 +0900

    Update 1. 계산기와 펜을 제작하시오.cpp

--- 1. 계산기와 펜을 제작하시오.cpp
+++ 1. 계산기와 펜을 제작하시오.cpp
@@ -12,13 +12,13 @@ void CTest1Dlg::OnBnClickedButton1() {
 
 CPoint oPnt;
 void CMFCApplication3Dlg::OnMouseMove(UINT nFlags, CPoint point) {
-	if (nFlags == MK_LBUTTON) {
-		CPen pen(PS_SOLID, 8, RGB(0, 255, 0));
-		CClientDC dc(this);
-		dc.SelectObject(&pen);
-		dc.MoveTo(oPnt);
-		dc.LineTo(point);
-	}
-	oPnt = point;  // 마우스 포인터의 업데이트
-	CDialogEx::OnMouseMove(nFlags, point);
+    if (nFlags == MK_LBUTTON) {
+        CPen pen(PS_SOLID, 8, RGB(0, 255, 0));
+	CClientDC dc(this);
+	dc.SelectObject(&pen);
+	dc.MoveTo(oPnt);
+	dc.LineTo(point);
+    }
+    oPnt = point;
+    CDialogEx::OnMouseMove(nFlags, point);
 }
